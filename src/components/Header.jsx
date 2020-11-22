a52d21c5c84806d0e67a01384bb1bb892009f9e8
Samuel Ruiz <SamuelPuckett77@gmail.com>
2020-11-21 21:40:05 -0800

    Mobile Responsiveness
    - fixed a bug where the location info box modal wouldn't appear as intended on mobile layouts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import locationIcon from '@iconify/icons-mdi/fire-alert';
 
 const Header = () => {
 	return (
-		<header className="navbar navbar-dark bg-dark">
+		<header
+			className="navbar navbar-dark bg-dark"
+			style={{ position: 'fixed', top: '0', right: '0', left: '0', zIndex: '100' }}
+		>
 			<h1 className="navbar-brand mx-auto align-middle">
 				Wildfire Tracker (Powered By NASA)
 			</h1>
